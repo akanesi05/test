de71c66551fa7c76415092d7d85adb6b618fdab5
mitsuiakane <akane.sumitomo@gmail.com>
2024-03-23 23:58:59 +0900

    5

---
+++ p20.rb
@@ -0,0 +1,12 @@
+# print_infoメソッドを定義してください
+def print_info(item)
+    puts "あっとへようこそ！"
+    puts "今日は#{item}がセール中です！"
+  end
+  
+  
+  print_info("ヘッドホン")
+  print_info("ストーブ")
+  # 引数を「テレビ」としてメソッドを呼び出してください
+  print_info("テレビ")
+  
